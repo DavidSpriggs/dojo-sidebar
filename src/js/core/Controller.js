e0bba3395b52ae023a447c3271f1702b3a548cb8
David Spriggs <spriggs.d@gmail.com>
2014-11-07 09:04:05 -0600

    Styled home and locate buttons.

--- src/js/core/Controller.js
+++ src/js/core/Controller.js
@@ -3,16 +3,15 @@ define([
   'esri/dijit/HomeButton',
   'esri/dijit/LocateButton',
   'esri/dijit/Geocoder',
+  'esri/dijit/BasemapToggle',
 
   'put-selector',
 
   'dojo/_base/lang',
   'dojo/domReady!'
 ], function(
-  Map, HomeButton, LocateButton, Geocoder,
-
+  Map, HomeButton, LocateButton, Geocoder, BasemapToggle,
   put,
-
   lang
 ) {
   return {
@@ -24,12 +23,12 @@ define([
       this.initMap();
     },
     initMap: function() {
-      this.map = new Map(put(document.body, 'div.map'), {
+      this.map = new Map(put(document.body, 'div.map'), { //create a div in the body and create an esri map in it
         center: [-56.049, 38.485],
         zoom: 3,
         basemap: 'streets'
       });
-      this.map.on('load', lang.hitch(this, 'initWidgets'));
+      this.map.on('load', lang.hitch(this, 'initWidgets')); //waint untill the map is loaded before creating
     },
     initWidgets: function() {
       //create controls div
@@ -43,15 +42,22 @@ define([
       this.home.startup();
 
       this.geoLocate = new LocateButton({
-        map: this.map
-      }, put(this.controlsNode, 'div.LocateButton div'));
+        map: this.map,
+        useTracking: false
+      }, put(this.controlsNode, 'div.locateButton div')); //create the locate button in the controls div
       this.geoLocate.startup();
 
       this.search = new Geocoder({
         map: this.map,
         autoComplete: true
-      }, put(this.controlsNode, 'div.search div'));
+      }, put(this.controlsNode, 'div.search div')); //create the search bar in the controls div
       this.search.startup();
+
+      this.basemaToggle = new BasemapToggle({
+        map: this.map,
+        basemap: 'hybrid'
+      }, put(this.map.root, 'div.basemapToggle div'));
+      this.basemaToggle.startup();
     }
   };
 });
